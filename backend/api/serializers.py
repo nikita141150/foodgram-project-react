45463648af87eceabfdf4180d1c66aeec2f01634
Никита <nikita141150@yandex.ru>
2022-05-18 23:37:36 +0300

    v0.4

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -39,9 +39,9 @@ class Hex2NameColor(serializers.Field):
     def to_internal_value(self, data):
         try:
             data = webcolors.hex_to_name(data)
+            return data
         except ValueError:
             raise serializers.ValidationError('Для этого цвета нет имени')
-        return data
 
 
 class TagSerializer(serializers.ModelSerializer):
