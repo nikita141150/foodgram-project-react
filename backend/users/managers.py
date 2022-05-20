ed33b6742665a4caec8bd4b2154da26afb833827
Никита <nikita141150@yandex.ru>
2022-05-20 19:42:28 +0300

    v0.4

--- backend/users/managers.py
+++ backend/users/managers.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.hashers import make_password
 
 
 class UserManager(BaseUserManager):
@@ -10,7 +9,7 @@ class UserManager(BaseUserManager):
             raise ValueError('Почта не может быть пустой!')
         email = self.normalize_email(email)
         user = self.model(email=email, username=username, **extra_fields)
-        user.password = make_password(password)
+        user.set_password(password)
         user.save()
         return user
 
